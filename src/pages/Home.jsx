3a4930226694f1b1482d9fd5a75ee70e3015705d
박성준 <freestarjun@gmail.com>
2025-12-29 17:28:11 +0900

    feat : Home창 개발

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,24 @@
-import {UseState} from 'react';
-import '../css/main.css'
+import { useState } from 'react'
+import '../styles/Home.css'
+
+
 
 function Home() {
+
     return (
         <>
-            <h1>왜</h1>
-        
-        
-        
+            <main className="Main-Title">
+                <p className = "Title">Point Check ✔️</p>
+            </main>
+
+            <div>
+                <button className="Check-Btn">체크하러가기</button>
+            </div>
+            <div>
+                <button className="Help-Btn"><img className="circle-questions" src="circle-questions.png" />도움말</button>
+            </div>
+
         </>
     )
-
 }
 export default Home
